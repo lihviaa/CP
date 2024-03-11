2830a8152fef775078f2a32a4e6299807736786c
Cloud User <ec2-user@ip-172-31-27-236.ec2.internal>
2024-03-11 02:24:17 +0000

    Adicionando paralelismo a partir de threads

--- lab02/v2lab02.c
+++ lab02/v2lab02.c
@@ -3,13 +3,79 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-int *result;
 int *array;
+int *result;
 int **matrix;
+int lines, columns;
 
-void populate(int* arg_array, int num_of_elements) {
-    for(int i = 0; i < num_of_elements; i++) {
-        arg_array[i] = rand() % 100;
+void *mult_mat_array(void* thread);
+void populate(int* arg_array, int num_elements);
+void print_array(int* arg_array, int num_elements);
+void print_matrix(int** arg_matrix, int num_lines, int num_columns);
+
+int main(int argc, char *argv[]) {
+    long thread_count;
+    if(argc != 3) {
+        fprintf(stderr, "Please, type number of lines and columns of matrix.\n");
+        return 1;
+    }
+
+    srand(time(NULL));
+
+    lines = atoi(argv[1]);
+    columns = atoi(argv[2]);
+
+    array = malloc(columns * sizeof(int));
+    populate(array, columns);
+    
+    matrix = malloc(lines * sizeof(int*));
+    for(int line = 0; line < lines; line++) {
+        matrix[line] = malloc(columns * sizeof(int));
+        populate(matrix[line], columns);
+    }
+
+    result = malloc(lines * sizeof(int));
+    pthread_t *threads = malloc(lines * sizeof(pthread_t));
+
+    for(thread_count = 0; thread_count < lines; thread_count++) {
+        pthread_create(&threads[thread_count], NULL, mult_mat_array, (void*) thread_count);
+    }
+
+    for(thread_count = 0; thread_count < lines; thread_count++) {
+        pthread_join(threads[thread_count], NULL);
+    }
+
+    print_matrix(matrix, lines, columns);
+    printf("\n");
+    print_array(array, columns);
+    printf("\n");
+
+    print_array(result, lines);
+    printf("\n");
+
+    for(int arrays = 0; arrays < lines; arrays++) {
+        free(matrix[arrays]);
+    }
+    free(matrix);
+    free(array);
+    free(result);
+    free(threads);
+
+    return 0;
+}
+
+void *mult_mat_array(void* thread) {
+    long thrd = (long) thread;
+    result[thrd] = 0;
+
+    for(int i = 0; i < columns; i++) {
+        result[thrd] += matrix[thrd][i] * array[i];
+    }
+}
+
+void populate(int* arg_array, int num_elements) {
+    for(int i = 0; i < num_elements; i++) {
+        arg_array[i] = rand() % 10;
     }
 }
 
@@ -37,33 +103,5 @@ void print_matrix(int** arg_matrix, int num_lines, int num_columns) {
     }
 
     printf(" }\n");
-}
-
-int main(int argc, char *argv[]) {
-    if(argc != 3) {
-        fprintf(stderr, "Please, type number of lines and columns of matrix.\n");
-        return 1;
-    }
-
-    srand(time(NULL));
-
-    int lines = atoi(argv[1]);
-    int columns = atoi(argv[2]);
-
-    array = malloc(columns * sizeof(int));
-    populate(array, columns);
-    
-    matrix = malloc(lines * sizeof(int*));
-    for(int line = 0; line < lines; line++) {
-        matrix[line] = malloc(columns * sizeof(int));
-        populate(matrix[line], columns);
-    }
-
-    print_matrix(matrix, lines, columns);
-    printf("\n");
-    print_array(array, columns);
-    printf("\n");
-
-    return 0;
 }
 
