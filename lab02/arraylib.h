929e749545136120c579c0c65f0c17182f0f1e1b
Cloud User <ec2-user@ip-172-31-27-236.ec2.internal>
2024-03-12 21:45:18 +0000

    Refatorando segunda versão

---
+++ lab02/arraylib.h
@@ -0,0 +1,58 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void rnd_populate(int *array, int elements, int range);
+int *rnd_array_alloc(int elements, int range);
+int **rnd_matrix_alloc(int lines, int columns, int range);
+void write_array_file(FILE *fpointer, int *array, int elements);
+void write_matrix_file(FILE *fpointer, int **matrix, int lines, int columns);
+
+void rnd_populate(int *array, int elements, int range) {
+    for(int i = 0; i < elements; i++) {
+        array[i] = rand() % range;
+    }
+}
+
+int *rnd_array_alloc(int elements, int range) {
+    int *array = (int*) malloc(elements * sizeof(int));
+    rnd_populate(array, elements, range);
+    
+    return array;
+}
+
+int **rnd_matrix_alloc(int lines, int columns, int range) {
+    int **matrix = (int**) malloc(lines * sizeof(int*));
+    for(int i = 0; i < lines; i++) {
+        matrix[i] = (int*) malloc(columns * sizeof(int));
+        rnd_populate(matrix[i], columns, range);
+    }
+    
+    return matrix;
+}
+
+void write_array_file(FILE *fpointer, int *array, int elements) {
+    fprintf(fpointer, "{ ");
+    for(int i = 0; i < elements; i++) {
+        fprintf(fpointer, "%02d", array[i]);
+
+        if(i != elements - 1) {
+            fprintf(fpointer, ", ");
+        }
+    }
+    
+    fprintf(fpointer, " }");
+}
+
+void write_matrix_file(FILE *fpointer, int **matrix, int lines, int columns) {
+    fprintf(fpointer, "{ ");
+    for(int j = 0; j < lines; j++) {
+        write_array_file(fpointer, matrix[j], columns);
+        
+        if(j != lines - 1) {
+            fprintf(fpointer, ",\n  ");
+        } 
+    }
+
+    fprintf(fpointer, " }\n");
+}
--- lab02/lab02.c
+++ lab02/lab02.c
@@ -1,73 +1,76 @@
-#include <pthread.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include "arraylib.h"
 
-int matrix[5][5] = {
-    {1, 2, 3, 4, 5},
-    {2, 3, 4, 5, 6},
-    {3, 4, 5, 6, 7},
-    {4, 5, 6, 7, 8},
-    {5, 6, 7, 8, 9}
-};
+int *array;
+int *result;
+int **matrix;
+int lines, columns;
 
-int array[5] = {1, 2, 3, 4, 5};
+void *par_mat_array(void* thread);
 
-int result[5];
+int main(int argc, char *argv[]) {
+    long thread_num;
+    srand(time(NULL));
+    FILE *presult, *parray, *pmatrix;
 
-void* Pth_mat_vect(void* threadNum) {
-    long resultElement = (long) threadNum;
-    int arrayLength = sizeof(array)/sizeof(int);
-
-    result[resultElement] = 0;
-    for(int i = 0; i < arrayLength; i++) {
-        result[resultElement] += matrix[resultElement][i] * array[i];
+    if(argc != 3) {
+        fprintf(stderr, "Please, type number of lines and columns of matrix.\n");
+        return 1;
     }
 
-    printf("Elemento de índice %ld calculado pela Thread %ld: %d\n", resultElement, threadNum, result[resultElement]);
+    lines = atoi(argv[1]);
+    columns = atoi(argv[2]);
 
-    return NULL;
-}
+    presult = fopen("result.txt", "w");
+    parray = fopen("array.txt", "w");
+    pmatrix = fopen("matrix.txt", "w");
 
-void printMatrix(int argLines, int argColumns, int argMatrix[argLines][argColumns]) {
-    for(int i = 0; i < argLines; i++) {
-        for(int j = 0; j < argColumns; j++) {
-            printf("%d ", argMatrix[i][j]);
-        }
-        printf("\n");
-    }
-}
+    array = rnd_array_alloc(columns, 10);
+    matrix = rnd_matrix_alloc(lines, columns, 10);
+    result = malloc(lines * sizeof(int));
 
-int main(void) {
-    int matrixLines = sizeof(matrix)/sizeof(matrix[0]);
-    int matrixColumns = sizeof(matrix[0])/sizeof(int);
-    pthread_t* threadArray = malloc(matrixLines * sizeof(pthread_t));
+    pthread_t *threads = malloc(lines * sizeof(pthread_t));
 
-    printf("Matriz exemplo: \n");
-    printMatrix(matrixLines, matrixColumns, matrix);
-    printf("\n");
-
-    printf("Vetor exemplo: ");
-    for(int l = 0; l < matrixLines; l++) {
-        printf("%d ", array[l]);
-    }
-    printf("\n\n");
-
-    for(long line = 0; line < matrixLines; line++) {
-        pthread_create(&threadArray[line], NULL, Pth_mat_vect, (void*) line);
+    for(thread_num = 0; thread_num < lines; thread_num++) {
+        pthread_create(&threads[thread_num], NULL, par_mat_array, (void*) thread_num);
     }
 
-    for(int i = 0; i < matrixLines; i++) {
-        pthread_join(threadArray[i], NULL);
+    for(thread_num = 0; thread_num < lines; thread_num++) {
+        pthread_join(threads[thread_num], NULL);
     }
 
-    printf("\nVetor resultante da multiplicação matriz-vetor: ");
-    for(int k = 0; k < matrixLines; k++) {
-	    printf("%d ", result[k]);
-    }
-    printf("\n");
+    write_matrix_file(pmatrix, matrix, lines, columns);
+    printf("Matrix written to matrix.txt\n");
+    write_array_file(parray, array, columns);
+    printf("Array written to array.txt\n");
+    write_array_file(presult, result, lines);
+    printf("Result written to result.txt\n");
 
-    free(threadArray);
+    for(int m = 0; m < lines; m++) free(matrix[m]);
+    free(matrix);
+    free(array);
+    free(result);
+    free(threads);
+    fclose(presult);
+    fclose(parray);
+    fclose(pmatrix);
 
     return 0;
+}
+
+void *par_mat_array(void* thread) {
+    long thread_id = pthread_self();
+    long thrd = (long) thread;
+    result[thrd] = 0;
+
+    for(int i = 0; i < columns; i++) {
+        result[thrd] += matrix[thrd][i] * array[i];
+    }
+
+    printf("Elemento de índice %ld calculado pela Thread %ld\n", thrd, thread_id);
+    return NULL;
 }
 
