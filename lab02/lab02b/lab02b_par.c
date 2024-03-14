5813bfe5aa2ca4adcb31eb159c4e267328db2707
Cloud User <ec2-user@ip-172-31-30-35.ec2.internal>
2024-03-14 22:19:59 +0000

    lab02b

---
+++ lab02/lab02b/lab02b_par.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include "arraylib.h"
+
+int *array;
+int *result;
+int **matrix;
+int lines, columns;
+
+void *par_mat_array(void* thread);
+
+int main(int argc, char *argv[]) {
+    long thrd_n;
+    srand(time(NULL));
+
+    if(argc < 3 || argc > 4) {
+        fprintf(stderr, "Please, check README before running file.\n");
+        return 1;
+    }
+
+    lines = atoi(argv[1]);
+    columns = atoi(argv[2]);
+    int prnt_flag = (argc == 4 && strcmp("-p", argv[3]) == 0) ? 1 : 0;
+
+    array = rnd_array_alloc(columns, 100);
+    matrix = rnd_matrix_alloc(lines, columns, 100);
+    result = (int*) malloc(lines * sizeof(int));
+
+    pthread_t *threads = malloc(lines * sizeof(pthread_t));
+
+    for(thrd_n = 0; thrd_n < lines; thrd_n++) {
+        pthread_create(&threads[thrd_n], NULL, par_mat_array, (void*) thrd_n);
+    }
+
+    for(thrd_n = 0; thrd_n < lines; thrd_n++) {
+        pthread_join(threads[thrd_n], NULL);
+    }
+
+    printf("\nParallel execution finished.\n");
+
+    if(prnt_flag) {
+        printf("\nRandom array generated:\n");
+        print_array(array, columns);
+        printf("\n\nRandom matrix generated:\n");
+        print_matrix(matrix, lines, columns);
+        printf("\nResulting array:\n");
+        print_array(result, lines);
+        printf("\n\n");
+    }
+
+    FILE *prll_result = fopen("result.txt", "w");
+    FILE *prll_array = fopen("array.txt", "w");
+    FILE *prll_matrix = fopen("matrix.txt", "w");
+
+    write_matrix_file(prll_matrix, matrix, lines, columns);
+    printf("Matrix written to matrix.txt\n");
+    write_array_file(prll_array, array, columns);
+    printf("Array written to array.txt\n");
+    write_array_file(prll_result, result, lines);
+    printf("Result written to result.txt\n");
+
+    for(int m = 0; m < lines; m++) free(matrix[m]);
+    free(matrix);
+    free(array);
+    free(result);
+    free(threads);
+    fclose(prll_array);
+    fclose(prll_result);
+    fclose(prll_matrix);
+    
+
+    return 0;
+}
+
+void *par_mat_array(void* thread) {
+    long thread_id = pthread_self();
+    long thrd = (long) thread;
+    result[thrd] = 0;
+
+    for(int i = 0; i < columns; i++) {
+        result[thrd] += matrix[thrd][i] * array[i];
+    }
+
+    printf("Parallel: Result[%ld] calculated by Thread %ld\n", thrd, thread_id);
+    return NULL;
+}
+
---
+++ lab02/lab02b/lab02b_srl.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "arraylib.h"
+
+void srl_mat_array(int **matrix, int *array, int *srl_result, int lines, int columns);
+
+int main(int argc, char *argv[]) {
+    if(argc < 3 || argc > 4) {
+        fprintf(stderr, "Usage: %s <lines> <columns> [-p]\n", argv[0]);
+        return 1;
+    }
+
+    int lines = atoi(argv[1]);
+    int columns = atoi(argv[2]);
+    int prnt_flag = (argc == 4 && strcmp("-p", argv[3]) == 0) ? 1 : 0;
+
+    int *array = rnd_array_alloc(columns, 100);
+    int **matrix = rnd_matrix_alloc(lines, columns, 100);
+    int *srl_result_array = (int*) malloc(lines * sizeof(int));
+
+    srl_mat_array(matrix, array, srl_result_array, lines, columns);
+
+    if(prnt_flag) {
+
+        printf("\nRandom array generated:\n");
+        print_array(array, columns);
+
+        printf("\n\nRandom matrix generated:\n");
+        print_matrix(matrix, lines, columns);
+
+        printf("\nResulting array:\n");
+        print_array(srl_result_array, lines);
+        printf("\n\n");
+        
+    }
+
+
+
+    FILE *array_file = fopen("array.txt", "w");
+    FILE *matrix_file = fopen("matrix.txt", "w");
+    FILE *srl_result_file = fopen("srl_result.txt", "w");
+
+    write_matrix_file(matrix_file, matrix, lines, columns);
+    printf("Matrix written to matrix.txt\n");
+
+    write_array_file(array_file, array, columns);
+    printf("Array written to array.txt\n");
+
+    write_array_file(srl_result_file, srl_result_array, lines);
+    printf("Result written to srl_result.txt\n");
+
+    
+
+    for(int i = 0; i < lines; i++) free(matrix[i]);
+    fclose(array_file);
+    fclose(matrix_file);
+    fclose(srl_result_file);
+    free(matrix);
+    free(array);
+    free(srl_result_array);
+
+    return 0;
+}
+
+void srl_mat_array(int **matrix, int *array, int *srl_result, int lines, int columns) {
+    for(int i = 0; i < lines; i++) {
+        srl_result[i] = 0;
+        for(int j = 0; j < columns; j++) {
+            srl_result[i] += matrix[i][j] * array[j];
+        }
+    }
+}
+
